3153caa917b7e22ddd5716f2fe0ab7c8f58a3cb2
Sergej Shumilin <shumilin845@gmail.com>
2018-06-07 11:47:58 +0400

    1

--- src/main/java/com/site/example/service/UserValidatorImpl.java
+++ src/main/java/com/site/example/service/UserValidatorImpl.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class UserValidatorImpl implements UserValidator {
     public boolean validate(String value){
-        int length = value.length();
+        int l = value.length();
         return length > 4 && length < 8;
     }
 }
