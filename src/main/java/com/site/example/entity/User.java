22d9c67ad5f08c648fb2716bd812c65a8314c5d9
Sergej Shumilin <shumilin845@gmail.com>
2018-06-12 12:29:49 +0400

    created method getByName

---
+++ src/main/java/com/site/example/entity/User.java
@@ -0,0 +1,52 @@
+package com.site.example.entity;
+
+import java.util.Objects;
+
+public class User {
+    String name;
+    String password;
+
+    public User(String name, String password) {
+        this.name = name;
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User entity = (User) o;
+        return Objects.equals(name, entity.name) &&
+                Objects.equals(password, entity.password);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(name, password);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
--- src/main/java/com/site/example/repository/UserRepositoryImpl.java
+++ src/main/java/com/site/example/repository/UserRepositoryImpl.java
@@ -7,8 +7,8 @@ import java.sql.*;
 public class UserRepositoryImpl implements UserRepository {
 
     private static final String url = "jdbc:mysql://localhost:3306/mydatabase?autoReconnect=true&useSSL=false";
-    private static final String userName = "root";
-    private static final String password = "12345";
+    private static final String name = "root";
+    private static final String passwordToDataBase = "12345";
     private static final String insertNew = "INSERT INTO people VALUES(?,?,?)";
 
     @Override
@@ -16,14 +16,13 @@ public class UserRepositoryImpl implements UserRepository {
         Connection connection = null;
         PreparedStatement preparedStatement = null;
         Driver driver = null;
-
         try {
             driver = new FabricMySQLDriver();
             DriverManager.registerDriver(driver);
-            connection = DriverManager.getConnection(url, userName, password);
+            connection = DriverManager.getConnection(url, name, passwordToDataBase);
             preparedStatement = connection.prepareStatement(insertNew);
-            preparedStatement.setString(2, "Alexandr");
-            preparedStatement.setString(3, "11111");
+            preparedStatement.setString(2, userName);
+            preparedStatement.setString(3, password);
             preparedStatement.execute();
         } catch (SQLException e) {
             e.printStackTrace();
@@ -34,9 +33,33 @@ public class UserRepositoryImpl implements UserRepository {
             } catch (SQLException e) {
                 e.printStackTrace();
             }
-
         }
     }
+
+    public String getByName(String userName) {
+
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        Driver driver = null;
+        try {
+            driver = new FabricMySQLDriver();
+            DriverManager.registerDriver(driver);
+            connection = DriverManager.getConnection(url, name, passwordToDataBase);
+            preparedStatement = connection.prepareStatement("SELECT password FROM mydatabase WHERE userName = ?");
+            preparedStatement.setString(1, userName);
+            preparedStatement.executeQuery();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                connection.close();
+                preparedStatement.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return userName;
     }
+}
 
 
